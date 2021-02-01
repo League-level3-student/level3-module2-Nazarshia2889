ca858f93b03dfbb97f5470bcd46a8cfd77b82af0
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2021-01-31 16:30:15 -0800

    done

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -11,12 +11,18 @@ class _04_SearchTest {
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
+		String[] words = {"apple", "banana", "orange", "grapes", "blackberry", "strawberries"};
+		assertEquals(1, _00_LinearSearch.linearSearch(words, "banana"));
+		assertEquals(3, _00_LinearSearch.linearSearch(words, "grapes"));
+		assertEquals(-1, _00_LinearSearch.linearSearch(words, "pear"));
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		int[] nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		assertEquals(8, _01_BinarySearch.binarySearch(nums, 0, nums.length-1, 8));
 	}
 	
 	@Test
